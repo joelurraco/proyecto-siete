a60396cc732056f8362a390ccf5d2ab218ae5427
joelurraco@gmail.com <joelurraco@gmail.com@6e14faa1-9237-017a-750e-7af3a8d7b343>
2011-03-16 14:18:42 +0000

    Añadido clase Enemigo, Personaje y Usuario

---
+++  proyecto-siete/WindowsFormsApplication1/Personaje.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoSIETE
+{
+    class Personaje
+    {
+        public
+            void setTipo(string tipo);
+            void setAtaque(int ataque);
+            void setVida(int vida);
+            void setDefensa(int defensa);
+
+            string getTipo();
+            int getAtaque();
+            int getVida();
+            int getDefensa();
+        private
+        	string tipo;
+	        int ataque;
+	        int vida;
+            int defensa;
+
+    }
+}
---
+++  proyecto-siete/WindowsFormsApplication1/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoSIETE
+{
+    class Usuario
+    {
+        private
+            void setNombre(string nombre);
+            void setClave(string clave);
+            void setIdPersonaje(int idPersonaje);
+            void setCorreo(string correo);
+            void setExperiencia(string experiencia);
+
+            int getId();
+            string getNombre();
+            string getClave();
+            int getIdPersonaje();
+            string getCorreo();
+            int getExperiencia();
+
+        public
+            int id;
+            string nombre;
+            string clave;
+            int idPersonaje;
+            string correo;
+            int experiencia;
+    }
+}
