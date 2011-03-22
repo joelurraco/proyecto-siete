724a0f43dd894a9ddd1590e985a624d181daabe9
joelurraco@gmail.com <joelurraco@gmail.com@6e14faa1-9237-017a-750e-7af3a8d7b343>
2011-03-22 10:31:33 +0000

    Añadida clase Objeto

--- trunk/ proyecto-siete/WindowsFormsApplication1/Enemigo.cs
+++ trunk/ proyecto-siete/WindowsFormsApplication1/Enemigo.cs
@@ -7,6 +7,27 @@ namespace ProyectoSIETE
 {
     class Enemigo
     {
-        
+        public
+            void setNombre(string nombre);
+            void setAtaque(int ataque);
+            void setVida(int vida);
+            void setDefensa(int defensa);
+            void setPuntos(int puntos);
+            void setImagen(string imagen);
+
+            string getNombre();
+            int getAtaque();
+            int getVida();
+            int getDefensa();
+            int getPuntos();
+            string getImagen();
+
+        private
+            string nombre;
+            int ataque;
+            int vida;
+            int defensa;
+            int puntos;
+            string imagen;
     }
 }
---
+++ trunk/ proyecto-siete/WindowsFormsApplication1/Objeto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoSIETE
+{
+    class Objeto
+    {
+        public
+            void setNombre(string nombre);
+            void setImagen(string imagen);
+
+            string getNombre();
+            string getImagen();
+        private
+            string nombre;
+            string imagen;
+    }
+}
