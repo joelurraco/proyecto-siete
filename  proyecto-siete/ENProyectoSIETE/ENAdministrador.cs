db08ae5f09278c4dcae870d14fab9407c293bf9d
joan.elche@gmail.com <joan.elche@gmail.com@6e14faa1-9237-017a-750e-7af3a8d7b343>
2011-03-30 12:31:24 +0000

    EN administrador

---
+++  proyecto-siete/ENProyectoSIETE/ENAdministrador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENProyectoSIETE
+{
+    class ENAdministrador
+    {
+        private int idAdministrador;
+        private string nombreAdministrador;
+        private string claveAdministrador;
+
+        public ENAdministrador(int id, string nombre, string clave)
+        {
+            idAdministrador = id;
+            nombreAdministrador = nombre;
+            claveAdministrador = clave;
+        }
+        public int IdAdministrador
+        {
+            get { return idAdministrador; }
+            set { idAdministrador = value; }
+        }
+        public string NombreAdministrador
+        {
+            get { return nombreAdministrador; }
+            set { nombreAdministrador = value; }
+        }
+        public string ClaveAdministrador
+        {
+            get { return claveAdministrador; }
+            set { claveAdministrador = value; }
+        }
+    }
+}
+
